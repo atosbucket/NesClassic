varunagarwal315/solidity

Add tests for the ErrorTag

test/libsolidity/SolidityEndToEndTest.cpp
@@ -7419,6 +7419,21 @@ BOOST_AUTO_TEST_CASE(inline_assembly_in_modifiers)
 	BOOST_CHECK(callContractFunction("f()") == encodeArgs(true));
 }
 
+BOOST_AUTO_TEST_CASE(inline_assembly_errortag)
+{
+	char const* sourceCode = R"(
+		contract A {
+			function f() {
+				assembly {
+					jump(ErrorTag)
+				}
+			}
+		}
+	)";
+	compileAndRun(sourceCode, 0, "C");
+	BOOST_CHECK(callContractFunction("f()") == encodeArgs());
+}
+
 BOOST_AUTO_TEST_SUITE_END()
 
 }

atosbucket/NesClassic

test/libsolidity/SolidityEndToEndTest.cpp
@@ -7692,6 +7692,21 @@ BOOST_AUTO_TEST_CASE(packed_storage_overflow)
 	BOOST_CHECK(callContractFunction("f()") == encodeArgs(u256(0x1234), u256(0), u256(0), u256(0xfffe)));
 }
 
+BOOST_AUTO_TEST_CASE(inline_assembly_errortag)
+{
+	char const* sourceCode = R"(
+		contract C {
+			function f() {
+				assembly {
+					jump(ErrorTag)
+				}
+			}
+		}
+	)";
+	compileAndRun(sourceCode, 0, "C");
+	BOOST_CHECK(callContractFunction("f()") == encodeArgs());
+}
+
 BOOST_AUTO_TEST_SUITE_END()
 
 }