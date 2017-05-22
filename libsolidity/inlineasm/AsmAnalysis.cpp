jamesray1/solidity

Support multiple variables in a variable declaration in inline assembly

libsolidity/inlineasm/AsmAnalysis.cpp
@@ -181,12 +181,32 @@ bool AsmAnalyzer::operator()(FunctionalAssignment const& _assignment)
 
 bool AsmAnalyzer::operator()(assembly::VariableDeclaration const& _varDecl)
 {
+	// The number of variable names and values must match. One exception
+	// is a single value, where a tuple assignment is assumed from a function.
+	if (_varDecl.names.size() != _varDecl.values.size())
+	{
+		if (_varDecl.values.size() != 1)
+		{
+			m_errors.push_back(make_shared<Error>(
+				Error::Type::DeclarationError,
+				"Variable declaration name and value count mismatch.",
+				_varDecl.location
+			));
+			return false;
+		}
+	}
+
+	int const expectedItems = _varDecl.names.size();
 	int const stackHeight = m_stackHeight;
-	bool success = boost::apply_visitor(*this, *_varDecl.value);
-	solAssert(m_stackHeight - stackHeight == 1, "Invalid value size.");
-	boost::get<Scope::Variable>(m_currentScope->identifiers.at(_varDecl.name)).active = true;
+	for (auto const& value: _varDecl.values)
+		if (!boost::apply_visitor(*this, value))
+			return false;
+	solAssert(m_stackHeight - stackHeight == expectedItems, "Invalid value size.");
+
+	for (auto const& name: _varDecl.names)
+		boost::get<Scope::Variable>(m_currentScope->identifiers.at(name)).active = true;
 	m_info.stackHeightInfo[&_varDecl] = m_stackHeight;
-	return success;
+	return true;
 }
 
 bool AsmAnalyzer::operator()(assembly::FunctionDefinition const& _funDef)

libsolidity/inlineasm/AsmParser.cpp
@@ -228,11 +228,25 @@ assembly::VariableDeclaration Parser::parseVariableDeclaration()
 {
 	VariableDeclaration varDecl = createWithLocation<VariableDeclaration>();
 	expectToken(Token::Let);
-	varDecl.name = expectAsmIdentifier();
+	while (true)
+	{
+		varDecl.names.push_back(expectAsmIdentifier());
+		if (m_scanner->currentToken() == Token::Comma)
+			expectToken(Token::Comma);
+		else
+			break;
+	}
 	expectToken(Token::Colon);
 	expectToken(Token::Assign);
-	varDecl.value.reset(new Statement(parseExpression()));
-	varDecl.location.end = locationOf(*varDecl.value).end;
+	while (true)
+	{
+		varDecl.values.emplace_back(new Statement(parseExpression()));
+		if (m_scanner->currentToken() == Token::Comma)
+			expectToken(Token::Comma);
+		else
+			break;
+	}
+	varDecl.location.end = locationOf(varDecl.values.back()).end;
 	return varDecl;
 }
 

libsolidity/inlineasm/AsmScopeFiller.cpp
@@ -59,7 +59,10 @@ bool ScopeFiller::operator()(Label const& _item)
 
 bool ScopeFiller::operator()(assembly::VariableDeclaration const& _varDecl)
 {
-	return registerVariable(_varDecl.name, _varDecl.location, *m_currentScope);
+	for (auto const& name: _varDecl.names)
+		if (!registerVariable(name, _varDecl.location, *m_currentScope))
+			return false;
+	return true;
 }
 
 bool ScopeFiller::operator()(assembly::FunctionDefinition const& _funDef)

atosbucket/NesClassic

libsolidity/inlineasm/AsmAnalysis.cpp
@@ -181,12 +181,32 @@ bool AsmAnalyzer::operator()(FunctionalAssignment const& _assignment)
 
 bool AsmAnalyzer::operator()(assembly::VariableDeclaration const& _varDecl)
 {
+	// The number of variable names and values must match. One exception
+	// is a single value, where a tuple assignment is assumed from a function.
+	if (_varDecl.variables.size() != _varDecl.values.size())
+	{
+		if (_varDecl.values.size() != 1)
+		{
+			m_errors.push_back(make_shared<Error>(
+				Error::Type::DeclarationError,
+				"Variable declaration name and value count mismatch.",
+				_varDecl.location
+			));
+			return false;
+		}
+	}
+
+	int const expectedItems = _varDecl.variables.size();
 	int const stackHeight = m_stackHeight;
-	bool success = boost::apply_visitor(*this, *_varDecl.value);
-	solAssert(m_stackHeight - stackHeight == 1, "Invalid value size.");
-	boost::get<Scope::Variable>(m_currentScope->identifiers.at(_varDecl.variable.name)).active = true;
+	for (auto const& value: _varDecl.values)
+		if (!boost::apply_visitor(*this, value))
+			return false;
+	solAssert(m_stackHeight - stackHeight == expectedItems, "Invalid value size.");
+
+	for (auto const& variable: _varDecl.variables)
+		boost::get<Scope::Variable>(m_currentScope->identifiers.at(variable.name)).active = true;
 	m_info.stackHeightInfo[&_varDecl] = m_stackHeight;
-	return success;
+	return true;
 }
 
 bool AsmAnalyzer::operator()(assembly::FunctionDefinition const& _funDef)

libsolidity/inlineasm/AsmParser.cpp
@@ -258,11 +258,25 @@ assembly::VariableDeclaration Parser::parseVariableDeclaration()
 {
 	VariableDeclaration varDecl = createWithLocation<VariableDeclaration>();
 	expectToken(Token::Let);
-	varDecl.variable = parseTypedName();
+	while (true)
+	{
+		varDecl.variables.push_back(parseTypedName());
+		if (m_scanner->currentToken() == Token::Comma)
+			expectToken(Token::Comma);
+		else
+			break;
+	}
 	expectToken(Token::Colon);
 	expectToken(Token::Assign);
-	varDecl.value.reset(new Statement(parseExpression()));
-	varDecl.location.end = locationOf(*varDecl.value).end;
+	while (true)
+	{
+		varDecl.values.emplace_back(new Statement(parseExpression()));
+		if (m_scanner->currentToken() == Token::Comma)
+			expectToken(Token::Comma);
+		else
+			break;
+	}
+	varDecl.location.end = locationOf(varDecl.values.back()).end;
 	return varDecl;
 }
 

libsolidity/inlineasm/AsmScopeFiller.cpp
@@ -59,7 +59,10 @@ bool ScopeFiller::operator()(Label const& _item)
 
 bool ScopeFiller::operator()(assembly::VariableDeclaration const& _varDecl)
 {
-	return registerVariable(_varDecl.variable, _varDecl.location, *m_currentScope);
+	for (auto const& variable: _varDecl.variables)
+		if (!registerVariable(variable, _varDecl.location, *m_currentScope))
+			return false;
+	return true;
 }
 
 bool ScopeFiller::operator()(assembly::FunctionDefinition const& _funDef)