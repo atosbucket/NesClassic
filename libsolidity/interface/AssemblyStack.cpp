wangwangwar/solidity

Supply text representation of assembly

libsolidity/interface/AssemblyStack.cpp
@@ -69,7 +69,9 @@ MachineAssemblyObject AssemblyStack::assemble(Machine _machine)
 		MachineAssemblyObject object;
 		auto assembly = assembly::CodeGenerator(m_errors).assemble(*m_parserResult, *m_analysisInfo);
 		object.binary = make_shared<eth::LinkerObject>(assembly.assemble());
-		/// TODO: fill out text representation
+		ostringstream tmp;
+		assembly.stream(tmp);
+		object.text = tmp.str();
 		return object;
 	}
 	case Machine::EVM15:

solc/CommandLineInterface.cpp
@@ -1101,6 +1101,10 @@ bool CommandLineInterface::assemble(
 			cout << object.binary.get()->toHex() << endl;
 		else
 			cerr << "No binary representation found." << endl;
+		if (!object.text.empty())
+			cout << object.text << endl;
+		else
+			cerr << "No text representation found." << endl;
 		cout << stack.print() << endl;
 	}
 

atosbucket/NesClassic

libsolidity/interface/AssemblyStack.cpp
@@ -91,7 +91,9 @@ MachineAssemblyObject AssemblyStack::assemble(Machine _machine) const
 		eth::Assembly assembly;
 		assembly::CodeGenerator::assemble(*m_parserResult, *m_analysisInfo, assembly);
 		object.bytecode = make_shared<eth::LinkerObject>(assembly.assemble());
-		/// TODO: fill out text representation
+		ostringstream tmp;
+		assembly.stream(tmp);
+		object.assembly = tmp.str();
 		return object;
 	}
 	case Machine::EVM15:

solc/CommandLineInterface.cpp
@@ -1101,6 +1101,10 @@ bool CommandLineInterface::assemble(
 			cout << object.bytecode->toHex() << endl;
 		else
 			cerr << "No binary representation found." << endl;
+		if (!object.assembly.empty())
+			cout << object.assembly << endl;
+		else
+			cerr << "No text representation found." << endl;
 		cout << stack.print() << endl;
 	}
 