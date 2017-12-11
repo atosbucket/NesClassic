Omosofe/solidity

Rename jsonCompiler to libsolc

scripts/bytecodecompare/storebytecode.sh
@@ -40,7 +40,7 @@ TMPDIR=$(mktemp -d)
 
     if [[ "$SOLC_EMSCRIPTEN" = "On" ]]
     then
-        cp "$REPO_ROOT/build/solc/soljson.js" .
+        cp "$REPO_ROOT/build/libsolc/libsolc.js" .
         npm install solc
         cat > solc <<EOF
 #!/usr/bin/env node

scripts/test_emscripten.sh
@@ -29,7 +29,7 @@
 set -e
 
 REPO_ROOT=$(cd $(dirname "$0")/.. && pwd)
-SOLJSON="$REPO_ROOT/build/solc/soljson.js"
+SOLJSON="$REPO_ROOT/build/libsolc/libsolc.js"
 
 DIR=$(mktemp -d)
 (

scripts/travis-emscripten/build_emscripten.sh
@@ -87,10 +87,10 @@ make -j 4
 
 cd ..
 mkdir -p upload
-cp build/solc/soljson.js upload/
-cp build/solc/soljson.js ./
+cp build/libsolc/libsolc.js upload/
+cp build/libsolc/libsolc.js ./
 
-OUTPUT_SIZE=`ls -la soljson.js`
+OUTPUT_SIZE=`ls -la libsolc.js`
 
 echo "Emscripten output size: $OUTPUT_SIZE"
 

atosbucket/NesClassic

scripts/bytecodecompare/storebytecode.sh
@@ -40,7 +40,7 @@ TMPDIR=$(mktemp -d)
 
     if [[ "$SOLC_EMSCRIPTEN" = "On" ]]
     then
-        cp "$REPO_ROOT/build/solc/soljson.js" .
+        cp "$REPO_ROOT/build/libsolc/soljson.js" .
         npm install solc
         cat > solc <<EOF
 #!/usr/bin/env node

scripts/test_emscripten.sh
@@ -29,7 +29,7 @@
 set -e
 
 REPO_ROOT=$(cd $(dirname "$0")/.. && pwd)
-SOLJSON="$REPO_ROOT/build/solc/soljson.js"
+SOLJSON="$REPO_ROOT/build/libsolc/soljson.js"
 
 DIR=$(mktemp -d)
 (

scripts/travis-emscripten/build_emscripten.sh
@@ -87,8 +87,8 @@ make -j 4
 
 cd ..
 mkdir -p upload
-cp build/solc/soljson.js upload/
-cp build/solc/soljson.js ./
+cp build/libsolc/soljson.js upload/
+cp build/libsolc/soljson.js ./
 
 OUTPUT_SIZE=`ls -la soljson.js`
 