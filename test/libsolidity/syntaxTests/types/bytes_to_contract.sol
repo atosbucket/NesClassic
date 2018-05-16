arun1595/solidity

Disallow conversions between bytesX and uintY of different size.

test/libsolidity/syntaxTests/types/bytes_to_contract.sol
@@ -1,7 +1,7 @@
 contract C {
         function f() public pure {
-                C(bytes20(0x1234));
+                C(bytes32(uint256(0x1234)));
         }
 }
 // ----
-// TypeError: (64-82): Explicit type conversion not allowed from "bytes20" to "contract C".
+// TypeError: (64-91): Explicit type conversion not allowed from "bytes32" to "contract C".

atosbucket/NesClassic

test/libsolidity/syntaxTests/types/bytes_to_contract.sol
@@ -1,7 +1,7 @@
 contract C {
         function f() public pure {
-                C(bytes20(0x1234));
+                C(bytes20(uint160(0x1234)));
         }
 }
 // ----
-// TypeError: (64-82): Explicit type conversion not allowed from "bytes20" to "contract C".
+// TypeError: (64-91): Explicit type conversion not allowed from "bytes20" to "contract C".