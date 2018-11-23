weisheme/solidity

Inject SMTLIB2 queries and responses via standard-json-io.

libsolidity/formal/SMTChecker.cpp
@@ -31,15 +31,15 @@ using namespace std;
 using namespace dev;
 using namespace dev::solidity;
 
-SMTChecker::SMTChecker(ErrorReporter& _errorReporter, ReadCallback::Callback const& _readFileCallback):
+SMTChecker::SMTChecker(ErrorReporter& _errorReporter, map<h256, string> const& _smtlib2Responses):
 #ifdef HAVE_Z3
 	m_interface(make_shared<smt::Z3Interface>()),
 #else
-	m_interface(make_shared<smt::SMTLib2Interface>(_readFileCallback)),
+	m_interface(make_shared<smt::SMTLib2Interface>(_smtlib2Responses)),
 #endif
 	m_errorReporter(_errorReporter)
 {
-	(void)_readFileCallback;
+	(void)_smtlib2Responses;
 }
 
 void SMTChecker::analyze(SourceUnit const& _source)
@@ -51,6 +51,11 @@ void SMTChecker::analyze(SourceUnit const& _source)
 		m_nextFreeSequenceCounter.clear();
 		_source.accept(*this);
 	}
+}
+
+vector<string> SMTChecker::unhandledQueries()
+{
+	return m_interface->unhandledQueries();
 }
 
 void SMTChecker::endVisit(VariableDeclaration const& _varDecl)

atosbucket/NesClassic

libsolidity/formal/SMTChecker.cpp
@@ -32,8 +32,8 @@ using namespace dev;
 using namespace langutil;
 using namespace dev::solidity;
 
-SMTChecker::SMTChecker(ErrorReporter& _errorReporter, ReadCallback::Callback const& _readFileCallback):
-	m_interface(make_shared<smt::SMTPortfolio>(_readFileCallback)),
+SMTChecker::SMTChecker(ErrorReporter& _errorReporter, map<h256, string> const& _smtlib2Responses):
+	m_interface(make_shared<smt::SMTPortfolio>(_smtlib2Responses)),
 	m_errorReporter(_errorReporter)
 {
 }