kojimaro/solidity

[Yul] Adds break/continue statements and some general tests for for-loop syntax.

libyul/AsmParser.cpp
@@ -23,6 +23,7 @@
 #include <libyul/AsmParser.h>
 #include <liblangutil/Scanner.h>
 #include <liblangutil/ErrorReporter.h>
+#include <libdevcore/Common.h>
 
 #include <boost/algorithm/string.hpp>
 
@@ -104,6 +105,32 @@ Statement Parser::parseStatement()
 	}
 	case Token::For:
 		return parseForLoop();
+	case Token::Break:
+		if (m_insideForLoopBody)
+		{
+			auto stmt = Statement{ createWithLocation<Break>() };
+			m_scanner->next();
+			return stmt;
+		}
+		else
+		{
+			m_errorReporter.syntaxError(location(), "Keyword break outside for-loop body is not allowed.");
+			m_scanner->next();
+			return {};
+		}
+	case Token::Continue:
+		if (m_insideForLoopBody)
+		{
+			auto stmt = Statement{ createWithLocation<Continue>() };
+			m_scanner->next();
+			return stmt;
+		}
+		else
+		{
+			m_errorReporter.syntaxError(location(), "Keyword continue outside for-loop body is not allowed.");
+			m_scanner->next();
+			return {};
+		}
 	case Token::Assign:
 	{
 		if (m_dialect->flavour != AsmFlavour::Loose)
@@ -243,13 +270,19 @@ Case Parser::parseCase()
 
 ForLoop Parser::parseForLoop()
 {
+	bool outsideForLoopBody = m_insideForLoopBody;
+	m_insideForLoopBody = false;
+
 	RecursionGuard recursionGuard(*this);
 	ForLoop forLoop = createWithLocation<ForLoop>();
 	expectToken(Token::For);
 	forLoop.pre = parseBlock();
 	forLoop.condition = make_unique<Expression>(parseExpression());
 	forLoop.post = parseBlock();
+
+	m_insideForLoopBody = true;
 	forLoop.body = parseBlock();
+	m_insideForLoopBody = outsideForLoopBody;
 	forLoop.location.end = forLoop.body.location.end;
 	return forLoop;
 }
@@ -455,6 +488,9 @@ VariableDeclaration Parser::parseVariableDeclaration()
 FunctionDefinition Parser::parseFunctionDefinition()
 {
 	RecursionGuard recursionGuard(*this);
+	auto outerForLoopBody = m_insideForLoopBody;
+	m_insideForLoopBody = false;
+	ScopeGuard restoreInsideForLoopBody{[&]() { m_insideForLoopBody = outerForLoopBody; }};
 	FunctionDefinition funDef = createWithLocation<FunctionDefinition>();
 	expectToken(Token::Function);
 	funDef.name = expectAsmIdentifier();

libyul/backends/evm/EVMCodeTransform.cpp
@@ -69,7 +69,6 @@ void VariableReferenceCounter::operator()(ForLoop const& _forLoop)
 	m_scope = originalScope;
 }
 
-
 void VariableReferenceCounter::operator()(Block const& _block)
 {
 	Scope* originalScope = m_scope;
@@ -91,7 +90,6 @@ void VariableReferenceCounter::increaseRefIfFound(YulString _variableName)
 		[=](Scope::Function const&) { }
 	));
 }
-
 
 CodeTransform::CodeTransform(
 	AbstractAssembly& _assembly,
@@ -605,11 +603,9 @@ void CodeTransform::operator()(ForLoop const& _forLoop)
 
 	visitStatements(_forLoop.pre.statements);
 
-	// TODO: When we implement break and continue, the labels and the stack heights at that point
-	// have to be stored in a stack.
 	AbstractAssembly::LabelID loopStart = m_assembly.newLabelId();
-	AbstractAssembly::LabelID loopEnd = m_assembly.newLabelId();
 	AbstractAssembly::LabelID postPart = m_assembly.newLabelId();
+	AbstractAssembly::LabelID loopEnd = m_assembly.newLabelId();
 
 	m_assembly.setSourceLocation(_forLoop.location);
 	m_assembly.appendLabel(loopStart);
@@ -619,6 +615,8 @@ void CodeTransform::operator()(ForLoop const& _forLoop)
 	m_assembly.appendInstruction(solidity::Instruction::ISZERO);
 	m_assembly.appendJumpToIf(loopEnd);
 
+	int const stackBodyHeight = m_assembly.stackHeight();
+	m_context->forLoopStack.emplace(Context::ForLoopLabels{ {postPart, stackBodyHeight}, {loopEnd, stackBodyHeight} });
 	(*this)(_forLoop.body);
 
 	m_assembly.setSourceLocation(_forLoop.location);
@@ -631,7 +629,19 @@ void CodeTransform::operator()(ForLoop const& _forLoop)
 	m_assembly.appendLabel(loopEnd);
 
 	finalizeBlock(_forLoop.pre, stackStartHeight);
+	m_context->forLoopStack.pop();
 	m_scope = originalScope;
+	checkStackHeight(&_forLoop);
+}
+
+void CodeTransform::operator()(Break const&)
+{
+	yulAssert(false, "Code generation for break statement in Yul is not implemented yet.");
+}
+
+void CodeTransform::operator()(Continue const&)
+{
+	yulAssert(false, "Code generation for continue statement in Yul is not implemented yet.");
 }
 
 void CodeTransform::operator()(Block const& _block)

libyul/backends/evm/EVMCodeTransform.h
@@ -30,6 +30,8 @@
 #include <boost/variant.hpp>
 #include <boost/optional.hpp>
 
+#include <stack>
+
 namespace langutil
 {
 class ErrorReporter;
@@ -57,6 +59,20 @@ struct CodeTransformContext
 	std::map<Scope::Function const*, AbstractAssembly::LabelID> functionEntryIDs;
 	std::map<Scope::Variable const*, int> variableStackHeights;
 	std::map<Scope::Variable const*, unsigned> variableReferences;
+
+	struct JumpInfo
+	{
+		AbstractAssembly::LabelID label;  ///< Jump's LabelID to jump to.
+		int stackHeightAdjust;            ///< Stack height after the jump.
+	};
+
+	struct ForLoopLabels
+	{
+		JumpInfo post; ///< Jump info for jumping to post branch.
+		JumpInfo done; ///< Jump info for jumping to done branch.
+	};
+
+	std::stack<ForLoopLabels> forLoopStack;
 };
 
 /**
@@ -166,6 +182,8 @@ public:
 	void operator()(Switch const& _switch);
 	void operator()(FunctionDefinition const&);
 	void operator()(ForLoop const&);
+	void operator()(Break const&);
+	void operator()(Continue const&);
 	void operator()(Block const& _block);
 
 private:

atosbucket/NesClassic

libyul/AsmParser.cpp
@@ -23,6 +23,7 @@
 #include <libyul/AsmParser.h>
 #include <liblangutil/Scanner.h>
 #include <liblangutil/ErrorReporter.h>
+#include <libdevcore/Common.h>
 
 #include <boost/algorithm/string.hpp>
 
@@ -104,6 +105,32 @@ Statement Parser::parseStatement()
 	}
 	case Token::For:
 		return parseForLoop();
+	case Token::Break:
+		if (m_insideForLoopBody)
+		{
+			auto stmt = Statement{ createWithLocation<Break>() };
+			m_scanner->next();
+			return stmt;
+		}
+		else
+		{
+			m_errorReporter.syntaxError(location(), "Keyword break outside for-loop body is not allowed.");
+			m_scanner->next();
+			return {};
+		}
+	case Token::Continue:
+		if (m_insideForLoopBody)
+		{
+			auto stmt = Statement{ createWithLocation<Continue>() };
+			m_scanner->next();
+			return stmt;
+		}
+		else
+		{
+			m_errorReporter.syntaxError(location(), "Keyword continue outside for-loop body is not allowed.");
+			m_scanner->next();
+			return {};
+		}
 	case Token::Assign:
 	{
 		if (m_dialect->flavour != AsmFlavour::Loose)
@@ -243,13 +270,19 @@ Case Parser::parseCase()
 
 ForLoop Parser::parseForLoop()
 {
+	bool outerForLoopBody = m_insideForLoopBody;
+	m_insideForLoopBody = false;
+
 	RecursionGuard recursionGuard(*this);
 	ForLoop forLoop = createWithLocation<ForLoop>();
 	expectToken(Token::For);
 	forLoop.pre = parseBlock();
 	forLoop.condition = make_unique<Expression>(parseExpression());
 	forLoop.post = parseBlock();
+
+	m_insideForLoopBody = true;
 	forLoop.body = parseBlock();
+	m_insideForLoopBody = outerForLoopBody;
 	forLoop.location.end = forLoop.body.location.end;
 	return forLoop;
 }
@@ -455,6 +488,9 @@ VariableDeclaration Parser::parseVariableDeclaration()
 FunctionDefinition Parser::parseFunctionDefinition()
 {
 	RecursionGuard recursionGuard(*this);
+	auto outerForLoopBody = m_insideForLoopBody;
+	m_insideForLoopBody = false;
+	ScopeGuard restoreInsideForLoopBody{[&]() { m_insideForLoopBody = outerForLoopBody; }};
 	FunctionDefinition funDef = createWithLocation<FunctionDefinition>();
 	expectToken(Token::Function);
 	funDef.name = expectAsmIdentifier();

libyul/backends/evm/EVMCodeTransform.cpp
@@ -69,7 +69,6 @@ void VariableReferenceCounter::operator()(ForLoop const& _forLoop)
 	m_scope = originalScope;
 }
 
-
 void VariableReferenceCounter::operator()(Block const& _block)
 {
 	Scope* originalScope = m_scope;
@@ -91,7 +90,6 @@ void VariableReferenceCounter::increaseRefIfFound(YulString _variableName)
 		[=](Scope::Function const&) { }
 	));
 }
-
 
 CodeTransform::CodeTransform(
 	AbstractAssembly& _assembly,
@@ -605,11 +603,9 @@ void CodeTransform::operator()(ForLoop const& _forLoop)
 
 	visitStatements(_forLoop.pre.statements);
 
-	// TODO: When we implement break and continue, the labels and the stack heights at that point
-	// have to be stored in a stack.
 	AbstractAssembly::LabelID loopStart = m_assembly.newLabelId();
-	AbstractAssembly::LabelID loopEnd = m_assembly.newLabelId();
 	AbstractAssembly::LabelID postPart = m_assembly.newLabelId();
+	AbstractAssembly::LabelID loopEnd = m_assembly.newLabelId();
 
 	m_assembly.setSourceLocation(_forLoop.location);
 	m_assembly.appendLabel(loopStart);
@@ -619,6 +615,8 @@ void CodeTransform::operator()(ForLoop const& _forLoop)
 	m_assembly.appendInstruction(solidity::Instruction::ISZERO);
 	m_assembly.appendJumpToIf(loopEnd);
 
+	int const stackHeightBody = m_assembly.stackHeight();
+	m_context->forLoopStack.emplace(Context::ForLoopLabels{ {postPart, stackHeightBody}, {loopEnd, stackHeightBody} });
 	(*this)(_forLoop.body);
 
 	m_assembly.setSourceLocation(_forLoop.location);
@@ -631,7 +629,19 @@ void CodeTransform::operator()(ForLoop const& _forLoop)
 	m_assembly.appendLabel(loopEnd);
 
 	finalizeBlock(_forLoop.pre, stackStartHeight);
+	m_context->forLoopStack.pop();
 	m_scope = originalScope;
+	checkStackHeight(&_forLoop);
+}
+
+void CodeTransform::operator()(Break const&)
+{
+	yulAssert(false, "Code generation for break statement in Yul is not implemented yet.");
+}
+
+void CodeTransform::operator()(Continue const&)
+{
+	yulAssert(false, "Code generation for continue statement in Yul is not implemented yet.");
 }
 
 void CodeTransform::operator()(Block const& _block)

libyul/backends/evm/EVMCodeTransform.h
@@ -30,6 +30,8 @@
 #include <boost/variant.hpp>
 #include <boost/optional.hpp>
 
+#include <stack>
+
 namespace langutil
 {
 class ErrorReporter;
@@ -57,6 +59,20 @@ struct CodeTransformContext
 	std::map<Scope::Function const*, AbstractAssembly::LabelID> functionEntryIDs;
 	std::map<Scope::Variable const*, int> variableStackHeights;
 	std::map<Scope::Variable const*, unsigned> variableReferences;
+
+	struct JumpInfo
+	{
+		AbstractAssembly::LabelID label;  ///< Jump's LabelID to jump to.
+		int targetStackHeight;            ///< Stack height after the jump.
+	};
+
+	struct ForLoopLabels
+	{
+		JumpInfo post; ///< Jump info for jumping to post branch.
+		JumpInfo done; ///< Jump info for jumping to done branch.
+	};
+
+	std::stack<ForLoopLabels> forLoopStack;
 };
 
 /**
@@ -166,6 +182,8 @@ public:
 	void operator()(Switch const& _switch);
 	void operator()(FunctionDefinition const&);
 	void operator()(ForLoop const&);
+	void operator()(Break const&);
+	void operator()(Continue const&);
 	void operator()(Block const& _block);
 
 private: