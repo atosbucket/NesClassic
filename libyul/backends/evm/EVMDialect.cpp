caballa/solidity-to-cmodel

Make dialect const& and allocate single instances statically.

libyul/backends/evm/EVMDialect.cpp
@@ -157,22 +157,34 @@ BuiltinFunctionForEVM const* EVMDialect::builtin(YulString _name) const
 		return nullptr;
 }
 
-shared_ptr<EVMDialect const> EVMDialect::looseAssemblyForEVM(langutil::EVMVersion _version)
+EVMDialect const& EVMDialect::looseAssemblyForEVM(langutil::EVMVersion _version)
 {
-	return make_shared<EVMDialect>(AsmFlavour::Loose, false, _version);
+	static map<langutil::EVMVersion, shared_ptr<EVMDialect const>> dialects;
+	if (!dialects[_version])
+		dialects[_version] = make_shared<EVMDialect>(AsmFlavour::Loose, false, _version);
+	return *dialects[_version];
 }
 
-shared_ptr<EVMDialect const> EVMDialect::strictAssemblyForEVM(langutil::EVMVersion _version)
+EVMDialect const& EVMDialect::strictAssemblyForEVM(langutil::EVMVersion _version)
 {
-	return make_shared<EVMDialect>(AsmFlavour::Strict, false, _version);
+	static map<langutil::EVMVersion, shared_ptr<EVMDialect const>> dialects;
+	if (!dialects[_version])
+		dialects[_version] = make_shared<EVMDialect>(AsmFlavour::Strict, false, _version);
+	return *dialects[_version];
 }
 
-shared_ptr<EVMDialect const> EVMDialect::strictAssemblyForEVMObjects(langutil::EVMVersion _version)
+EVMDialect const& EVMDialect::strictAssemblyForEVMObjects(langutil::EVMVersion _version)
 {
-	return make_shared<EVMDialect>(AsmFlavour::Strict, true, _version);
+	static map<langutil::EVMVersion, shared_ptr<EVMDialect const>> dialects;
+	if (!dialects[_version])
+		dialects[_version] = make_shared<EVMDialect>(AsmFlavour::Strict, true, _version);
+	return *dialects[_version];
 }
 
-shared_ptr<EVMDialect const> EVMDialect::yulForEVM(langutil::EVMVersion _version)
+EVMDialect const& EVMDialect::yulForEVM(langutil::EVMVersion _version)
 {
-	return make_shared<EVMDialect>(AsmFlavour::Yul, false, _version);
+	static map<langutil::EVMVersion, shared_ptr<EVMDialect const>> dialects;
+	if (!dialects[_version])
+		dialects[_version] = make_shared<EVMDialect>(AsmFlavour::Yul, false, _version);
+	return *dialects[_version];
 }

atosbucket/NesClassic

libyul/backends/evm/EVMDialect.cpp
@@ -138,22 +138,34 @@ BuiltinFunctionForEVM const* EVMDialect::builtin(YulString _name) const
 		return nullptr;
 }
 
-shared_ptr<EVMDialect const> EVMDialect::looseAssemblyForEVM(langutil::EVMVersion _version)
+EVMDialect const& EVMDialect::looseAssemblyForEVM(langutil::EVMVersion _version)
 {
-	return make_shared<EVMDialect>(AsmFlavour::Loose, false, _version);
+	static map<langutil::EVMVersion, unique_ptr<EVMDialect const>> dialects;
+	if (!dialects[_version])
+		dialects[_version] = make_unique<EVMDialect>(AsmFlavour::Loose, false, _version);
+	return *dialects[_version];
 }
 
-shared_ptr<EVMDialect const> EVMDialect::strictAssemblyForEVM(langutil::EVMVersion _version)
+EVMDialect const& EVMDialect::strictAssemblyForEVM(langutil::EVMVersion _version)
 {
-	return make_shared<EVMDialect>(AsmFlavour::Strict, false, _version);
+	static map<langutil::EVMVersion, unique_ptr<EVMDialect const>> dialects;
+	if (!dialects[_version])
+		dialects[_version] = make_unique<EVMDialect>(AsmFlavour::Strict, false, _version);
+	return *dialects[_version];
 }
 
-shared_ptr<EVMDialect const> EVMDialect::strictAssemblyForEVMObjects(langutil::EVMVersion _version)
+EVMDialect const& EVMDialect::strictAssemblyForEVMObjects(langutil::EVMVersion _version)
 {
-	return make_shared<EVMDialect>(AsmFlavour::Strict, true, _version);
+	static map<langutil::EVMVersion, unique_ptr<EVMDialect const>> dialects;
+	if (!dialects[_version])
+		dialects[_version] = make_unique<EVMDialect>(AsmFlavour::Strict, true, _version);
+	return *dialects[_version];
 }
 
-shared_ptr<EVMDialect const> EVMDialect::yulForEVM(langutil::EVMVersion _version)
+EVMDialect const& EVMDialect::yulForEVM(langutil::EVMVersion _version)
 {
-	return make_shared<EVMDialect>(AsmFlavour::Yul, false, _version);
+	static map<langutil::EVMVersion, unique_ptr<EVMDialect const>> dialects;
+	if (!dialects[_version])
+		dialects[_version] = make_unique<EVMDialect>(AsmFlavour::Yul, false, _version);
+	return *dialects[_version];
 }