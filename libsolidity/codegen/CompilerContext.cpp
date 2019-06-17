mastereuclid/solidity

Make Yul optimizer not fail for wasm.

libsolidity/codegen/CompilerContext.cpp
@@ -32,8 +32,8 @@
 #include <libyul/AsmAnalysisInfo.h>
 #include <libyul/backends/evm/AsmCodeGen.h>
 #include <libyul/backends/evm/EVMDialect.h>
+#include <libyul/backends/evm/Metrics.h>
 #include <libyul/optimiser/Suite.h>
-#include <libyul/optimiser/Metrics.h>
 #include <libyul/YulString.h>
 
 #include <liblangutil/ErrorReporter.h>

libyul/AssemblyStack.cpp
@@ -31,6 +31,7 @@
 #include <libyul/backends/evm/EVMCodeTransform.h>
 #include <libyul/backends/evm/EVMDialect.h>
 #include <libyul/backends/evm/EVMObjectCompiler.h>
+#include <libyul/backends/evm/Metrics.h>
 #include <libyul/backends/wasm/WasmDialect.h>
 #include <libyul/backends/wasm/EWasmObjectCompiler.h>
 #include <libyul/optimiser/Metrics.h>
@@ -146,15 +147,17 @@ void AssemblyStack::optimize(Object& _object, bool _isCreation)
 	for (auto& subNode: _object.subObjects)
 		if (auto subObject = dynamic_cast<Object*>(subNode.get()))
 			optimize(*subObject, false);
-	EVMDialect const& dialect = dynamic_cast<EVMDialect const&>(languageToDialect(m_language, m_evmVersion));
-	GasMeter meter(dialect, _isCreation, m_optimiserSettings.expectedExecutionsPerDeployment);
-	OptimiserSuite::run(
-		dialect,
-		meter,
-		*_object.code,
-		*_object.analysisInfo,
-		m_optimiserSettings.optimizeStackAllocation
-	);
+	if (EVMDialect const* dialect = dynamic_cast<EVMDialect const*>(&languageToDialect(m_language, m_evmVersion)))
+	{
+		GasMeter meter(*dialect, _isCreation, m_optimiserSettings.expectedExecutionsPerDeployment);
+		OptimiserSuite::run(
+			*dialect,
+			meter,
+			*_object.code,
+			*_object.analysisInfo,
+			m_optimiserSettings.optimizeStackAllocation
+		);
+	}
 }
 
 MachineAssemblyObject AssemblyStack::assemble(Machine _machine) const

libyul/backends/evm/ConstantOptimiser.cpp
@@ -18,14 +18,12 @@
  * Optimisation stage that replaces constants by expressions that compute them.
  */
 
-#include <libyul/optimiser/ConstantOptimiser.h>
+#include <libyul/backends/evm/ConstantOptimiser.h>
 
 #include <libyul/optimiser/ASTCopier.h>
-#include <libyul/optimiser/Metrics.h>
+#include <libyul/backends/evm/Metrics.h>
 #include <libyul/AsmData.h>
-#include <libyul/AsmPrinter.h>
 #include <libyul/Utilities.h>
-#include <libyul/AsmParser.h>
 
 #include <libdevcore/CommonData.h>
 

test/libyul/YulOptimizerTest.cpp
@@ -23,7 +23,6 @@
 #include <libyul/optimiser/VarDeclInitializer.h>
 #include <libyul/optimiser/VarNameCleaner.h>
 #include <libyul/optimiser/ControlFlowSimplifier.h>
-#include <libyul/optimiser/ConstantOptimiser.h>
 #include <libyul/optimiser/DeadCodeEliminator.h>
 #include <libyul/optimiser/Disambiguator.h>
 #include <libyul/optimiser/CommonSubexpressionEliminator.h>
@@ -47,8 +46,9 @@
 #include <libyul/optimiser/StructuralSimplifier.h>
 #include <libyul/optimiser/StackCompressor.h>
 #include <libyul/optimiser/Suite.h>
-#include <libyul/optimiser/Metrics.h>
+#include <libyul/backends/evm/ConstantOptimiser.h>
 #include <libyul/backends/evm/EVMDialect.h>
+#include <libyul/backends/evm/Metrics.h>
 #include <libyul/backends/wasm/WordSizeTransform.h>
 #include <libyul/AsmPrinter.h>
 #include <libyul/AsmParser.h>

atosbucket/NesClassic

libsolidity/codegen/CompilerContext.cpp
@@ -32,8 +32,8 @@
 #include <libyul/AsmAnalysisInfo.h>
 #include <libyul/backends/evm/AsmCodeGen.h>
 #include <libyul/backends/evm/EVMDialect.h>
+#include <libyul/backends/evm/EVMMetrics.h>
 #include <libyul/optimiser/Suite.h>
-#include <libyul/optimiser/Metrics.h>
 #include <libyul/YulString.h>
 
 #include <liblangutil/ErrorReporter.h>

libyul/AssemblyStack.cpp
@@ -31,6 +31,7 @@
 #include <libyul/backends/evm/EVMCodeTransform.h>
 #include <libyul/backends/evm/EVMDialect.h>
 #include <libyul/backends/evm/EVMObjectCompiler.h>
+#include <libyul/backends/evm/EVMMetrics.h>
 #include <libyul/backends/wasm/WasmDialect.h>
 #include <libyul/backends/wasm/EWasmObjectCompiler.h>
 #include <libyul/optimiser/Metrics.h>
@@ -146,15 +147,17 @@ void AssemblyStack::optimize(Object& _object, bool _isCreation)
 	for (auto& subNode: _object.subObjects)
 		if (auto subObject = dynamic_cast<Object*>(subNode.get()))
 			optimize(*subObject, false);
-	EVMDialect const& dialect = dynamic_cast<EVMDialect const&>(languageToDialect(m_language, m_evmVersion));
-	GasMeter meter(dialect, _isCreation, m_optimiserSettings.expectedExecutionsPerDeployment);
-	OptimiserSuite::run(
-		dialect,
-		meter,
-		*_object.code,
-		*_object.analysisInfo,
-		m_optimiserSettings.optimizeStackAllocation
-	);
+	if (EVMDialect const* dialect = dynamic_cast<EVMDialect const*>(&languageToDialect(m_language, m_evmVersion)))
+	{
+		GasMeter meter(*dialect, _isCreation, m_optimiserSettings.expectedExecutionsPerDeployment);
+		OptimiserSuite::run(
+			*dialect,
+			meter,
+			*_object.code,
+			*_object.analysisInfo,
+			m_optimiserSettings.optimizeStackAllocation
+		);
+	}
 }
 
 MachineAssemblyObject AssemblyStack::assemble(Machine _machine) const

libyul/backends/evm/ConstantOptimiser.cpp
@@ -18,14 +18,12 @@
  * Optimisation stage that replaces constants by expressions that compute them.
  */
 
-#include <libyul/optimiser/ConstantOptimiser.h>
+#include <libyul/backends/evm/ConstantOptimiser.h>
 
 #include <libyul/optimiser/ASTCopier.h>
-#include <libyul/optimiser/Metrics.h>
+#include <libyul/backends/evm/EVMMetrics.h>
 #include <libyul/AsmData.h>
-#include <libyul/AsmPrinter.h>
 #include <libyul/Utilities.h>
-#include <libyul/AsmParser.h>
 
 #include <libdevcore/CommonData.h>
 

test/libyul/YulOptimizerTest.cpp
@@ -23,7 +23,6 @@
 #include <libyul/optimiser/VarDeclInitializer.h>
 #include <libyul/optimiser/VarNameCleaner.h>
 #include <libyul/optimiser/ControlFlowSimplifier.h>
-#include <libyul/optimiser/ConstantOptimiser.h>
 #include <libyul/optimiser/DeadCodeEliminator.h>
 #include <libyul/optimiser/Disambiguator.h>
 #include <libyul/optimiser/CommonSubexpressionEliminator.h>
@@ -47,8 +46,9 @@
 #include <libyul/optimiser/StructuralSimplifier.h>
 #include <libyul/optimiser/StackCompressor.h>
 #include <libyul/optimiser/Suite.h>
-#include <libyul/optimiser/Metrics.h>
+#include <libyul/backends/evm/ConstantOptimiser.h>
 #include <libyul/backends/evm/EVMDialect.h>
+#include <libyul/backends/evm/EVMMetrics.h>
 #include <libyul/backends/wasm/WordSizeTransform.h>
 #include <libyul/AsmPrinter.h>
 #include <libyul/AsmParser.h>