Khushal17ad/solidity

Implement multi source semantic tests

test/ExecutionFramework.h
@@ -68,6 +68,15 @@ public:
 		std::map<std::string, Address> const& _libraryAddresses = std::map<std::string, Address>()
 	) = 0;
 
+	virtual bytes const& multiSourceCompileAndRunWithoutCheck(
+		std::map<std::string, std::string> const& _sources,
+		u256 const& _value = 0,
+		std::string const& _contractName = "",
+		bytes const& _arguments = bytes(),
+		std::map<std::string, Address> const& _libraryAddresses = std::map<std::string, Address>()
+	) = 0;
+
+
 	bytes const& compileAndRun(
 		std::string const& _sourceCode,
 		u256 const& _value = 0,
@@ -76,7 +85,13 @@ public:
 		std::map<std::string, Address> const& _libraryAddresses = std::map<std::string, Address>()
 	)
 	{
-		compileAndRunWithoutCheck(_sourceCode, _value, _contractName, _arguments, _libraryAddresses);
+		multiSourceCompileAndRunWithoutCheck(
+			std::map<std::string, std::string>({{ "", _sourceCode }}),
+			_value,
+			_contractName,
+			_arguments,
+			_libraryAddresses
+		);
 		BOOST_REQUIRE(m_transactionSuccessful);
 		BOOST_REQUIRE(!m_output.empty());
 		return m_output;

test/TestCaseReader.h
@@ -23,6 +23,16 @@
 
 namespace solidity::frontend::test
 {
+
+/**
+ * A map for registering source names that also contains the last source name in a test case.
+ */
+struct SourceMap
+{
+	std::map<std::string, std::string> sourceMap;
+	std::string mainSourceFile;
+};
+
 /**
  * A reader for test case data file, which parses source, settings and (optionally) simple expectations.
  */
@@ -32,7 +42,7 @@ public:
 	TestCaseReader() = default;
 	explicit TestCaseReader(std::string const& _filename);
 
-	std::map<std::string, std::string> const& sources() { return m_sources; }
+	SourceMap const& sources() { return m_sources; }
 	std::string const& source();
 	std::size_t lineNumber() { return m_lineNumber; }
 	std::map<std::string, std::string> const& settings() { return m_settings; }
@@ -47,11 +57,11 @@ public:
 	void ensureAllSettingsRead() const;
 
 private:
-	std::pair<std::map<std::string, std::string>, std::size_t> parseSourcesAndSettingsWithLineNumber(std::istream& _file);
+	std::pair<SourceMap, std::size_t> parseSourcesAndSettingsWithLineNumber(std::istream& _file);
 	static std::string parseSimpleExpectations(std::istream& _file);
 
 	std::ifstream m_file;
-	std::map<std::string, std::string> m_sources;
+	SourceMap m_sources;
 	std::size_t m_lineNumber = 0;
 	std::map<std::string, std::string> m_settings;
 	std::map<std::string, std::string> m_unreadSettings; ///< tracks which settings are left unread

test/libsolidity/SemanticTest.h
@@ -58,9 +58,8 @@ public:
 	/// Compiles and deploys currently held source.
 	/// Returns true if deployment was successful, false otherwise.
 	bool deploy(std::string const& _contractName, u256 const& _value, bytes const& _arguments, std::map<std::string, solidity::test::Address> const& _libraries = {});
-
 private:
-	std::string m_source;
+	SourceMap m_source;
 	std::size_t m_lineOffset;
 	std::vector<TestFunctionCall> m_tests;
 	bool m_runWithYul = false;

test/libsolidity/SolidityExecutionFramework.cpp
@@ -31,22 +31,46 @@ using namespace solidity::frontend;
 using namespace solidity::frontend::test;
 using namespace std;
 
+bytes SolidityExecutionFramework::multiSourceCompileContract(
+	map<string, string> const& _sourceCode,
+	string const& _contractName,
+	map<string, Address> const& _libraryAddresses
+)
+{
+	map<string, string> sourceCodeMap = _sourceCode;
+	for (auto &entry: sourceCodeMap)
+		addPreamble(entry.second);
+
+	return compileContractCommon(
+		sourceCodeMap,
+		_contractName,
+		_libraryAddresses
+	);
+}
+
 bytes SolidityExecutionFramework::compileContract(
 	string const& _sourceCode,
 	string const& _contractName,
 	map<string, Address> const& _libraryAddresses
 )
 {
-	// Silence compiler version warning
-	std::string sourceCode = "pragma solidity >=0.0;\n";
-	if (
-		solidity::test::CommonOptions::get().useABIEncoderV2 &&
-		_sourceCode.find("pragma experimental ABIEncoderV2;") == std::string::npos
-	)
-		sourceCode += "pragma experimental ABIEncoderV2;\n";
-	sourceCode += _sourceCode;
+	string sourceCode = _sourceCode;
+	addPreamble(sourceCode);
+	return compileContractCommon(
+		{{ "", sourceCode }},
+		_contractName,
+		_libraryAddresses
+	);
+}
+
+bytes SolidityExecutionFramework::compileContractCommon(
+	map<string, string> const& _sourceCodeMap,
+	string const& _contractName,
+	map<string, Address> const& _libraryAddresses
+)
+{
 	m_compiler.reset();
-	m_compiler.setSources({{"", sourceCode}});
+	m_compiler.setSources(_sourceCodeMap);
 	m_compiler.setLibraries(_libraryAddresses);
 	m_compiler.setRevertStringBehaviour(m_revertStrings);
 	m_compiler.setEVMVersion(m_evmVersion);
@@ -84,4 +108,16 @@ bytes SolidityExecutionFramework::compileContract(
 	if (m_showMetadata)
 		cout << "metadata: " << m_compiler.metadata(contractName) << endl;
 	return obj.bytecode;
+}
+
+void SolidityExecutionFramework::addPreamble(string& _sourceCode)
+{
+	// Silence compiler version warning
+	string preamble = "pragma solidity >=0.0;\n";
+	if (
+		solidity::test::CommonOptions::get().useABIEncoderV2 &&
+		_sourceCode.find("pragma experimental ABIEncoderV2;") == string::npos
+	)
+		preamble += "pragma experimental ABIEncoderV2;\n";
+	_sourceCode = preamble + _sourceCode;
 }

atosbucket/NesClassic

Co-authored-by: chriseth <chris@ethereum.org>
Co-authored-by: Kamil Śliwak <kamil.sliwak@codepoets.it>

test/ExecutionFramework.h
@@ -61,22 +61,28 @@ public:
 	virtual ~ExecutionFramework() = default;
 
 	virtual bytes const& compileAndRunWithoutCheck(
-		std::string const& _sourceCode,
+		std::map<std::string, std::string> const& _sourceCode,
 		u256 const& _value = 0,
 		std::string const& _contractName = "",
-		bytes const& _arguments = bytes(),
-		std::map<std::string, Address> const& _libraryAddresses = std::map<std::string, Address>()
+		bytes const& _arguments = {},
+		std::map<std::string, Address> const& _libraryAddresses = {}
 	) = 0;
 
 	bytes const& compileAndRun(
 		std::string const& _sourceCode,
 		u256 const& _value = 0,
 		std::string const& _contractName = "",
-		bytes const& _arguments = bytes(),
-		std::map<std::string, Address> const& _libraryAddresses = std::map<std::string, Address>()
+		bytes const& _arguments = {},
+		std::map<std::string, Address> const& _libraryAddresses = {}
 	)
 	{
-		compileAndRunWithoutCheck(_sourceCode, _value, _contractName, _arguments, _libraryAddresses);
+		compileAndRunWithoutCheck(
+			{{"", _sourceCode}},
+			_value,
+			_contractName,
+			_arguments,
+			_libraryAddresses
+		);
 		BOOST_REQUIRE(m_transactionSuccessful);
 		BOOST_REQUIRE(!m_output.empty());
 		return m_output;
@@ -293,4 +299,3 @@ protected:
 
 
 } // end namespaces
-

test/TestCaseReader.h
@@ -23,6 +23,16 @@
 
 namespace solidity::frontend::test
 {
+
+/**
+ * A map for registering source names that also contains the main source name in a test case.
+ */
+struct SourceMap
+{
+	std::map<std::string, std::string> sources;
+	std::string mainSourceFile;
+};
+
 /**
  * A reader for test case data file, which parses source, settings and (optionally) simple expectations.
  */
@@ -32,10 +42,10 @@ public:
 	TestCaseReader() = default;
 	explicit TestCaseReader(std::string const& _filename);
 
-	std::map<std::string, std::string> const& sources() { return m_sources; }
-	std::string const& source();
-	std::size_t lineNumber() { return m_lineNumber; }
-	std::map<std::string, std::string> const& settings() { return m_settings; }
+	SourceMap const& sources() const { return m_sources; }
+	std::string const& source() const;
+	std::size_t lineNumber() const { return m_lineNumber; }
+	std::map<std::string, std::string> const& settings() const { return m_settings; }
 	std::ifstream& stream() { return m_file; }
 
 	std::string simpleExpectations();
@@ -47,11 +57,11 @@ public:
 	void ensureAllSettingsRead() const;
 
 private:
-	std::pair<std::map<std::string, std::string>, std::size_t> parseSourcesAndSettingsWithLineNumber(std::istream& _file);
+	std::pair<SourceMap, std::size_t> parseSourcesAndSettingsWithLineNumber(std::istream& _file);
 	static std::string parseSimpleExpectations(std::istream& _file);
 
 	std::ifstream m_file;
-	std::map<std::string, std::string> m_sources;
+	SourceMap m_sources;
 	std::size_t m_lineNumber = 0;
 	std::map<std::string, std::string> m_settings;
 	std::map<std::string, std::string> m_unreadSettings; ///< tracks which settings are left unread

test/libsolidity/SemanticTest.h
@@ -58,9 +58,8 @@ public:
 	/// Compiles and deploys currently held source.
 	/// Returns true if deployment was successful, false otherwise.
 	bool deploy(std::string const& _contractName, u256 const& _value, bytes const& _arguments, std::map<std::string, solidity::test::Address> const& _libraries = {});
-
 private:
-	std::string m_source;
+	SourceMap m_sources;
 	std::size_t m_lineOffset;
 	std::vector<TestFunctionCall> m_tests;
 	bool m_runWithYul = false;

test/libsolidity/SolidityExecutionFramework.cpp
@@ -31,22 +31,18 @@ using namespace solidity::frontend;
 using namespace solidity::frontend::test;
 using namespace std;
 
-bytes SolidityExecutionFramework::compileContract(
-	string const& _sourceCode,
+bytes SolidityExecutionFramework::multiSourceCompileContract(
+	map<string, string> const& _sourceCode,
 	string const& _contractName,
 	map<string, Address> const& _libraryAddresses
 )
 {
-	// Silence compiler version warning
-	std::string sourceCode = "pragma solidity >=0.0;\n";
-	if (
-		solidity::test::CommonOptions::get().useABIEncoderV2 &&
-		_sourceCode.find("pragma experimental ABIEncoderV2;") == std::string::npos
-	)
-		sourceCode += "pragma experimental ABIEncoderV2;\n";
-	sourceCode += _sourceCode;
+	map<string, string> sourcesWithPreamble = _sourceCode;
+	for (auto& entry: sourcesWithPreamble)
+		entry.second = addPreamble(entry.second);
+
 	m_compiler.reset();
-	m_compiler.setSources({{"", sourceCode}});
+	m_compiler.setSources(sourcesWithPreamble);
 	m_compiler.setLibraries(_libraryAddresses);
 	m_compiler.setRevertStringBehaviour(m_revertStrings);
 	m_compiler.setEVMVersion(m_evmVersion);
@@ -84,4 +80,29 @@ bytes SolidityExecutionFramework::compileContract(
 	if (m_showMetadata)
 		cout << "metadata: " << m_compiler.metadata(contractName) << endl;
 	return obj.bytecode;
+}
+
+bytes SolidityExecutionFramework::compileContract(
+	string const& _sourceCode,
+	string const& _contractName,
+	map<string, Address> const& _libraryAddresses
+)
+{
+	return multiSourceCompileContract(
+		{{"", _sourceCode}},
+		_contractName,
+		_libraryAddresses
+	);
+}
+
+string SolidityExecutionFramework::addPreamble(string const& _sourceCode)
+{
+	// Silence compiler version warning
+	string preamble = "pragma solidity >=0.0;\n";
+	if (
+		solidity::test::CommonOptions::get().useABIEncoderV2 &&
+		_sourceCode.find("pragma experimental ABIEncoderV2;") == string::npos
+	)
+		preamble += "pragma experimental ABIEncoderV2;\n";
+	return preamble + _sourceCode;
 }