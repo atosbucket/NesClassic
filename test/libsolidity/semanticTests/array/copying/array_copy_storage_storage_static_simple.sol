EthWarrior/solidity

[Sol->Yul] Fixing array clearing when copying from storage to storage.

test/libsolidity/semanticTests/array/copying/array_copy_storage_storage_static_simple.sol
@@ -1,0 +1,15 @@
+contract c {
+    byte[2] data1;
+    bytes2[2] data2;
+    function test() public returns (bytes2, bytes2) {
+        uint i;
+        for (i = 0; i < data1.length; ++i)
+            data1[i] = byte(uint8(1 + i));
+        data2 = data1;
+        return (data2[0], data2[1]);
+    }
+}
+// ====
+// compileViaYul: also
+// ----
+// test() -> left(0x01), left(0x02)

atosbucket/NesClassic

test/libsolidity/semanticTests/array/copying/array_copy_storage_storage_static_simple.sol
@@ -1,0 +1,15 @@
+contract C {
+    bytes1[2] data1;
+    bytes2[2] data2;
+    function test() public returns (bytes2, bytes2) {
+        uint i;
+        for (i = 0; i < data1.length; ++i)
+            data1[i] = bytes1(uint8(1 + i));
+        data2 = data1;
+        return (data2[0], data2[1]);
+    }
+}
+// ====
+// compileViaYul: also
+// ----
+// test() -> left(0x01), left(0x02)