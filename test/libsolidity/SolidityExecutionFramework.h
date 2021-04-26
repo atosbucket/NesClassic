03-65/solidity

[isoltest] Add support for external sources.

test/libsolidity/SolidityExecutionFramework.h
@@ -49,10 +49,11 @@ public:
 		u256 const& _value = 0,
 		std::string const& _contractName = "",
 		bytes const& _arguments = {},
-		std::map<std::string, solidity::test::Address> const& _libraryAddresses = {}
+		std::map<std::string, solidity::test::Address> const& _libraryAddresses = {},
+		std::optional<std::string> const& _sourceName = std::nullopt
 	) override
 	{
-		bytes bytecode = multiSourceCompileContract(_sourceCode, _contractName, _libraryAddresses);
+		bytes bytecode = multiSourceCompileContract(_sourceCode, _sourceName, _contractName, _libraryAddresses);
 		sendMessage(bytecode + _arguments, true, _value);
 		return m_output;
 	}
@@ -65,6 +66,7 @@ public:
 
 	bytes multiSourceCompileContract(
 		std::map<std::string, std::string> const& _sources,
+		std::optional<std::string> const& _sourceName = std::nullopt,
 		std::string const& _contractName = "",
 		std::map<std::string, solidity::test::Address> const& _libraryAddresses = {}
 	);

atosbucket/NesClassic

test/libsolidity/SolidityExecutionFramework.h
@@ -49,10 +49,11 @@ public:
 		u256 const& _value = 0,
 		std::string const& _contractName = "",
 		bytes const& _arguments = {},
-		std::map<std::string, solidity::test::Address> const& _libraryAddresses = {}
+		std::map<std::string, solidity::test::Address> const& _libraryAddresses = {},
+		std::optional<std::string> const& _sourceName = std::nullopt
 	) override
 	{
-		bytes bytecode = multiSourceCompileContract(_sourceCode, _contractName, _libraryAddresses);
+		bytes bytecode = multiSourceCompileContract(_sourceCode, _sourceName, _contractName, _libraryAddresses);
 		sendMessage(bytecode + _arguments, true, _value);
 		return m_output;
 	}
@@ -65,6 +66,7 @@ public:
 
 	bytes multiSourceCompileContract(
 		std::map<std::string, std::string> const& _sources,
+		std::optional<std::string> const& _mainSourceName = std::nullopt,
 		std::string const& _contractName = "",
 		std::map<std::string, solidity::test::Address> const& _libraryAddresses = {}
 	);