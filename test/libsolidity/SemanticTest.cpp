ethereum/solidity

[isoltest] Replace storage command with 'storage_empty' builtin.

test/libsolidity/SemanticTest.cpp
@@ -31,6 +31,7 @@
 #include <optional>
 #include <stdexcept>
 #include <utility>
+#include <functional>
 
 using namespace std;
 using namespace solidity;
@@ -52,6 +53,8 @@ SemanticTest::SemanticTest(string const& _filename, langutil::EVMVersion _evmVer
 	m_lineOffset(m_reader.lineNumber()),
 	m_enforceViaYul(enforceViaYul)
 {
+	initializeBuiltins();
+
 	string choice = m_reader.stringSetting("compileViaYul", "default");
 	if (choice == "also")
 	{
@@ -105,6 +108,15 @@ SemanticTest::SemanticTest(string const& _filename, langutil::EVMVersion _evmVer
 
 	parseExpectations(m_reader.stream());
 	soltestAssert(!m_tests.empty(), "No tests specified in " + _filename);
+}
+
+void SemanticTest::initializeBuiltins()
+{
+	m_builtins["storage_empty"] = [this](FunctionCall const& _call) -> std::optional<bytes>
+	{
+	  soltestAssert(_call.arguments.parameters.empty(), "No arguments expected.");
+	  return toBigEndian(u256(storageEmpty(m_contractAddress) ? 1 : 0));
+	};
 }
 
 TestCase::TestResult SemanticTest::run(ostream& _stream, string const& _linePrefix, bool _formatted)
@@ -190,16 +202,7 @@ TestCase::TestResult SemanticTest::runTest(
 			constructed = true;
 		}
 
-		if (test.call().kind == FunctionCall::Kind::Storage)
-		{
-			test.setFailure(false);
-			bytes result(1, !storageEmpty(m_contractAddress));
-			test.setRawBytes(result);
-			soltestAssert(test.call().expectations.rawBytes().size() == 1, "");
-			if (test.call().expectations.rawBytes() != result)
-				success = false;
-		}
-		else if (test.call().kind == FunctionCall::Kind::Constructor)
+		if (test.call().kind == FunctionCall::Kind::Constructor)
 		{
 			if (m_transactionSuccessful == test.call().expectations.failure)
 				success = false;

atosbucket/NesClassic

test/libsolidity/SemanticTest.cpp
@@ -27,6 +27,7 @@
 #include <algorithm>
 #include <cctype>
 #include <fstream>
+#include <functional>
 #include <memory>
 #include <optional>
 #include <stdexcept>
@@ -123,6 +124,12 @@ void SemanticTest::initializeBuiltins()
 	m_builtins["smokeTest"] = [](FunctionCall const&) -> std::optional<bytes>
 	{
 		return util::toBigEndian(u256(0x1234));
+	};
+	soltestAssert(m_builtins.count("storageEmpty") == 0, "");
+	m_builtins["storageEmpty"] = [this](FunctionCall const& _call) -> std::optional<bytes>
+	{
+		soltestAssert(_call.arguments.parameters.empty(), "No arguments expected.");
+		return toBigEndian(u256(storageEmpty(m_contractAddress) ? 1 : 0));
 	};
 }
 
@@ -222,16 +229,7 @@ TestCase::TestResult SemanticTest::runTest(
 			constructed = true;
 		}
 
-		if (test.call().kind == FunctionCall::Kind::Storage)
-		{
-			test.setFailure(false);
-			bytes result(1, !storageEmpty(m_contractAddress));
-			test.setRawBytes(result);
-			soltestAssert(test.call().expectations.rawBytes().size() == 1, "");
-			if (test.call().expectations.rawBytes() != result)
-				success = false;
-		}
-		else if (test.call().kind == FunctionCall::Kind::Constructor)
+		if (test.call().kind == FunctionCall::Kind::Constructor)
 		{
 			if (m_transactionSuccessful == test.call().expectations.failure)
 				success = false;