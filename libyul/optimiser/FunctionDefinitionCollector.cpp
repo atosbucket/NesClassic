zointblackbriar/solidity

Function definition collector.

libyul/optimiser/FunctionDefinitionCollector.cpp
@@ -1,0 +1,36 @@
+/*
+	This file is part of solidity.
+
+	solidity is free software: you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
+
+	solidity is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with solidity.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include <libyul/optimiser/FunctionDefinitionCollector.h>
+#include <libyul/AsmData.h>
+
+using namespace std;
+using namespace solidity;
+using namespace solidity::yul;
+
+map<YulString, FunctionDefinition const*> FunctionDefinitionCollector::run(Block& _block)
+{
+	FunctionDefinitionCollector functionDefinitionCollector;
+	functionDefinitionCollector(_block);
+	return functionDefinitionCollector.m_functionDefinitions;
+}
+
+void FunctionDefinitionCollector::operator()(FunctionDefinition const& _functionDefinition)
+{
+	m_functionDefinitions[_functionDefinition.name] = &_functionDefinition;
+	ASTWalker::operator()(_functionDefinition);
+}

atosbucket/NesClassic

libyul/optimiser/FunctionDefinitionCollector.cpp
@@ -1,0 +1,36 @@
+/*
+	This file is part of solidity.
+
+	solidity is free software: you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
+
+	solidity is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with solidity.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include <libyul/optimiser/FunctionDefinitionCollector.h>
+#include <libyul/AST.h>
+
+using namespace std;
+using namespace solidity;
+using namespace solidity::yul;
+
+map<YulString, FunctionDefinition const*> FunctionDefinitionCollector::run(Block& _block)
+{
+	FunctionDefinitionCollector functionDefinitionCollector;
+	functionDefinitionCollector(_block);
+	return functionDefinitionCollector.m_functionDefinitions;
+}
+
+void FunctionDefinitionCollector::operator()(FunctionDefinition const& _functionDefinition)
+{
+	m_functionDefinitions[_functionDefinition.name] = &_functionDefinition;
+	ASTWalker::operator()(_functionDefinition);
+}