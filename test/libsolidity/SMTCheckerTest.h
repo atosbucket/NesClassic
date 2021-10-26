ryzheboka/solidity

Add SMTCheckerTest isoltest option to ignore invariants

test/libsolidity/SMTCheckerTest.h
@@ -49,6 +49,8 @@ protected:
 		Set in m_modelCheckerSettings.
 	SMTIgnoreCex: `yes`, `no`, where the default is `no`.
 		Set in m_ignoreCex.
+	SMTIgnoreInv: `yes`, `no`, where the default is `no`.
+		Set in m_ignoreInv.
 	SMTShowUnproved: `yes`, `no`, where the default is `yes`.
 		Set in m_modelCheckerSettings.
 	SMTSolvers: `all`, `cvc4`, `z3`, `none`, where the default is `all`.
@@ -58,6 +60,7 @@ protected:
 	ModelCheckerSettings m_modelCheckerSettings;
 
 	bool m_ignoreCex = false;
+	bool m_ignoreInv = false;
 };
 
 }

atosbucket/NesClassic

test/libsolidity/SMTCheckerTest.h
@@ -49,6 +49,8 @@ protected:
 		Set in m_modelCheckerSettings.
 	SMTIgnoreCex: `yes`, `no`, where the default is `no`.
 		Set in m_ignoreCex.
+	SMTIgnoreInv: `yes`, `no`, where the default is `no`.
+		Set in m_modelCheckerSettings.
 	SMTShowUnproved: `yes`, `no`, where the default is `yes`.
 		Set in m_modelCheckerSettings.
 	SMTSolvers: `all`, `cvc4`, `z3`, `none`, where the default is `all`.