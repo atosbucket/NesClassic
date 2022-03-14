msooseth/solidity

Support compilation fail scenarios in ASTJSON tests

test/libsolidity/ASTJSONTest.h
@@ -90,6 +90,7 @@ private:
 	) const;
 
 	std::vector<TestVariant> m_variants;
+	std::optional<CompilerStack::State> m_expectedFailState;
 
 	std::vector<std::pair<std::string, std::string>> m_sources;
 };

atosbucket/NesClassic

test/libsolidity/ASTJSONTest.h
@@ -90,6 +90,7 @@ private:
 	) const;
 
 	std::vector<TestVariant> m_variants;
+	std::optional<CompilerStack::State> m_expectedFailAfter;
 
 	std::vector<std::pair<std::string, std::string>> m_sources;
 };