jay-Anonymous/NdovuBlockchainApp

added optimization. shl comes out from being distributed over and

test/formal/and_distributed_over_shl.py
@@ -1,0 +1,25 @@
+from opcodes import AND, SHL
+from rule import Rule
+from z3 import BitVec
+
+"""
+Rule:
+AND(SHL(A,X), SHL(A,Y)) -> SHL(A, AND(X,Y))
+"""
+
+rule = Rule()
+
+n_bits = 128
+
+# Input vars
+X = BitVec('X', n_bits)
+Y = BitVec('Y', n_bits)
+A = BitVec('A', n_bits)
+
+# Non optimized result
+nonopt = AND(SHL(A,X), SHL(A,Y))
+
+# Optimized result
+opt = SHL(A, AND(X,Y))
+
+rule.check(nonopt, opt)

atosbucket/NesClassic

test/formal/and_distributed_over_shl.py
@@ -1,0 +1,25 @@
+from opcodes import AND, SHL
+from rule import Rule
+from z3 import BitVec
+
+"""
+Rule:
+AND(SHL(Z,X), SHL(Z,Y)) -> SHL(Z, AND(X,Y))
+"""
+
+rule = Rule()
+
+n_bits = 128
+
+# Input vars
+X = BitVec('X', n_bits)
+Y = BitVec('Y', n_bits)
+Z = BitVec('Z', n_bits)
+
+# Non optimized result
+nonopt = AND(SHL(Z,X), SHL(Z,Y))
+
+# Optimized result
+opt = SHL(Z, AND(X,Y))
+
+rule.check(nonopt, opt)