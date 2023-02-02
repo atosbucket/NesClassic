Shaw2666/solidity

Allow library external functions to be bound with using for

test/libsolidity/syntaxTests/using/library_functions_attached_in_single_directive_at_file_level.sol
@@ -1,0 +1,17 @@
+library L {
+    function publicFunction(uint) public pure {}
+    function externalFunction(uint) external pure {}
+    function internalFunction(uint) internal pure {}
+}
+
+using {L.publicFunction, L.externalFunction, L.internalFunction} for uint;
+
+contract C {
+    function f() public pure {
+        uint x;
+        x.publicFunction();
+        x.externalFunction();
+        x.internalFunction();
+    }
+}
+// ----

test/libsolidity/syntaxTests/using/library_functions_attached_in_single_directive_inside_contract.sol
@@ -1,0 +1,17 @@
+library L {
+    function publicFunction(uint) public pure {}
+    function externalFunction(uint) external pure {}
+    function internalFunction(uint) internal pure {}
+}
+
+contract C {
+    using {L.publicFunction, L.externalFunction, L.internalFunction} for uint;
+
+    function f() public pure {
+        uint x;
+        x.publicFunction();
+        x.externalFunction();
+        x.internalFunction();
+    }
+}
+// ----

atosbucket/NesClassic

test/libsolidity/syntaxTests/using/library_functions_attached_in_single_directive_at_file_level.sol
@@ -1,0 +1,17 @@
+library L {
+    function externalFunction(uint) external pure {}
+    function publicFunction(uint) public pure {}
+    function internalFunction(uint) internal pure {}
+}
+
+using {L.externalFunction, L.publicFunction, L.internalFunction} for uint;
+
+contract C {
+    function f() public pure {
+        uint x;
+        x.externalFunction();
+        x.publicFunction();
+        x.internalFunction();
+    }
+}
+// ----

test/libsolidity/syntaxTests/using/library_functions_attached_in_single_directive_inside_contract.sol
@@ -1,0 +1,17 @@
+library L {
+    function externalFunction(uint) external pure {}
+    function publicFunction(uint) public pure {}
+    function internalFunction(uint) internal pure {}
+}
+
+contract C {
+    using {L.externalFunction, L.publicFunction, L.internalFunction} for uint;
+
+    function f() public pure {
+        uint x;
+        x.externalFunction();
+        x.publicFunction();
+        x.internalFunction();
+    }
+}
+// ----