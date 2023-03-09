wecreateproject/solidity

Report safe properties in BMC and CHC

libsolidity/formal/BMC.cpp
@@ -99,6 +99,26 @@ void BMC::analyze(SourceUnit const& _source, map<ASTNode const*, set<Verificatio
 			" Consider increasing the timeout per query."
 		);
 
+	if (!m_settings.showProvedSafe && !m_safeTargets.empty())
+		m_errorReporter.info(
+			6002_error,
+			"BMC: " +
+			to_string(m_safeTargets.size()) +
+			" verification condition(s) proved safe!" +
+			" Enable the model checker option \"show proved safe\" to see all of them."
+		);
+	else if (m_settings.showProvedSafe)
+		for (auto const& [node, targets]: m_safeTargets)
+			for (auto const& target: targets)
+				m_errorReporter.info(
+					2961_error,
+					node->location(),
+					"BMC: " +
+					targetDescription(target) +
+					" check is safe!"
+				);
+
+
 	// If this check is true, Z3 and CVC4 are not available
 	// and the query answers were not provided, since SMTPortfolio
 	// guarantees that SmtLib2Interface is the first solver, if enabled.
@@ -694,6 +714,34 @@ pair<vector<smtutil::Expression>, vector<string>> BMC::modelExpressions()
 
 /// Verification targets.
 
+string BMC::targetDescription(BMCVerificationTarget const& target)
+{
+	if (
+		target.type == VerificationTargetType::Underflow ||
+		target.type == VerificationTargetType::Overflow
+	)
+	{
+		auto const* intType = dynamic_cast<IntegerType const*>(target.expression->annotation().type);
+		if (!intType)
+			intType = TypeProvider::uint256();
+
+		if (target.type == VerificationTargetType::Underflow)
+			return "Underflow (resulting value less than " + formatNumberReadable(intType->minValue()) + ")";
+		else if (target.type == VerificationTargetType::Overflow)
+			return "Overflow (resulting value larger than " + formatNumberReadable(intType->maxValue()) + ")";
+		else
+			solAssert(false);
+	}
+	else if (target.type == VerificationTargetType::DivByZero)
+		return "Division by zero";
+	else if (target.type == VerificationTargetType::Assert)
+		return "Assertion violation";
+	else if (target.type == VerificationTargetType::Balance)
+		return "Insufficient funds";
+	else
+		solAssert(false);
+}
+
 void BMC::checkVerificationTargets()
 {
 	for (auto& target: m_verificationTargets)
@@ -762,13 +810,13 @@ void BMC::checkUnderflow(BMCVerificationTarget& _target)
 		intType = TypeProvider::uint256();
 
 	checkCondition(
+		_target,
 		_target.constraints && _target.value < smt::minValue(*intType),
 		_target.callStack,
 		_target.modelExpressions,
 		_target.expression->location(),
 		4144_error,
 		8312_error,
-		"Underflow (resulting value less than " + formatNumberReadable(intType->minValue()) + ")",
 		"<result>",
 		&_target.value
 	);
@@ -795,13 +843,13 @@ void BMC::checkOverflow(BMCVerificationTarget& _target)
 		intType = TypeProvider::uint256();
 
 	checkCondition(
+		_target,
 		_target.constraints && _target.value > smt::maxValue(*intType),
 		_target.callStack,
 		_target.modelExpressions,
 		_target.expression->location(),
 		2661_error,
 		8065_error,
-		"Overflow (resulting value larger than " + formatNumberReadable(intType->maxValue()) + ")",
 		"<result>",
 		&_target.value
 	);
@@ -818,13 +866,13 @@ void BMC::checkDivByZero(BMCVerificationTarget& _target)
 		return;
 
 	checkCondition(
+		_target,
 		_target.constraints && (_target.value == 0),
 		_target.callStack,
 		_target.modelExpressions,
 		_target.expression->location(),
 		3046_error,
 		5272_error,
-		"Division by zero",
 		"<result>",
 		&_target.value
 	);
@@ -834,13 +882,13 @@ void BMC::checkBalance(BMCVerificationTarget& _target)
 {
 	solAssert(_target.type == VerificationTargetType::Balance, "");
 	checkCondition(
+		_target,
 		_target.constraints && _target.value,
 		_target.callStack,
 		_target.modelExpressions,
 		_target.expression->location(),
 		1236_error,
 		4010_error,
-		"Insufficient funds",
 		"address(this).balance"
 	);
 }
@@ -856,13 +904,13 @@ void BMC::checkAssert(BMCVerificationTarget& _target)
 		return;
 
 	checkCondition(
+		_target,
 		_target.constraints && !_target.value,
 		_target.callStack,
 		_target.modelExpressions,
 		_target.expression->location(),
 		4661_error,
-		7812_error,
-		"Assertion violation"
+		7812_error
 	);
 }
 
@@ -894,13 +942,13 @@ void BMC::addVerificationTarget(
 /// Solving.
 
 void BMC::checkCondition(
+	BMCVerificationTarget const& _target,
 	smtutil::Expression _condition,
 	vector<SMTEncoder::CallStackEntry> const& _callStack,
 	pair<vector<smtutil::Expression>, vector<string>> const& _modelExpressions,
 	SourceLocation const& _location,
 	ErrorId _errorHappens,
 	ErrorId _errorMightHappen,
-	string const& _description,
 	string const& _additionalValueName,
 	smtutil::Expression const* _additionalValue
 )
@@ -942,7 +990,7 @@ void BMC::checkCondition(
 	{
 		solAssert(!_callStack.empty(), "");
 		std::ostringstream message;
-		message << "BMC: " << _description << " happens here.";
+		message << "BMC: " << targetDescription(_target) << " happens here.";
 
 		std::ostringstream modelMessage;
 		// Sometimes models have complex smtlib2 expressions that SMTLib2Interface fails to parse.
@@ -969,12 +1017,15 @@ void BMC::checkCondition(
 		break;
 	}
 	case smtutil::CheckResult::UNSATISFIABLE:
+	{
+		m_safeTargets[_target.expression].insert(_target);
 		break;
+	}
 	case smtutil::CheckResult::UNKNOWN:
 	{
 		++m_unprovedAmt;
 		if (m_settings.showUnproved)
-			m_errorReporter.warning(_errorMightHappen, _location, "BMC: " + _description + " might happen here.", secondaryLocation);
+			m_errorReporter.warning(_errorMightHappen, _location, "BMC: " + targetDescription(_target) + " might happen here.", secondaryLocation);
 		break;
 	}
 	case smtutil::CheckResult::CONFLICTING:

libsolidity/formal/BMC.h
@@ -135,7 +135,14 @@ private:
 		Expression const* expression;
 		std::vector<CallStackEntry> callStack;
 		std::pair<std::vector<smtutil::Expression>, std::vector<std::string>> modelExpressions;
+
+		friend bool operator<(BMCVerificationTarget const& a, BMCVerificationTarget const& b)
+		{
+			return a.expression->id() <= b.expression->id();
+		}
 	};
+
+	std::string targetDescription(BMCVerificationTarget const& target);
 
 	void checkVerificationTargets();
 	void checkVerificationTarget(BMCVerificationTarget& _target);
@@ -156,13 +163,13 @@ private:
 	//@{
 	/// Check that a condition can be satisfied.
 	void checkCondition(
+		BMCVerificationTarget const& _target,
 		smtutil::Expression _condition,
 		std::vector<CallStackEntry> const& _callStack,
 		std::pair<std::vector<smtutil::Expression>, std::vector<std::string>> const& _modelExpressions,
 		langutil::SourceLocation const& _location,
 		langutil::ErrorId _errorHappens,
 		langutil::ErrorId _errorMightHappen,
-		std::string const& _description,
 		std::string const& _additionalValueName = "",
 		smtutil::Expression const* _additionalValue = nullptr
 	);
@@ -188,7 +195,10 @@ private:
 
 	std::vector<BMCVerificationTarget> m_verificationTargets;
 
-	/// Targets that were already proven.
+	/// Targets proved safe by this engine.
+	std::map<ASTNode const*, std::set<BMCVerificationTarget>, smt::EncodingContext::IdCompare> m_safeTargets;
+
+	/// Targets that were already proven before this engine started.
 	std::map<ASTNode const*, std::set<VerificationTargetType>, smt::EncodingContext::IdCompare> m_solvedTargets;
 
 	/// Number of verification conditions that could not be proved.

libsolidity/formal/CHC.h
@@ -65,13 +65,25 @@ public:
 
 	void analyze(SourceUnit const& _sources);
 
+	struct CHCVerificationTarget: VerificationTarget
+	{
+		unsigned const errorId;
+		ASTNode const* const errorNode;
+
+		friend bool operator<(CHCVerificationTarget const& a, CHCVerificationTarget const& b)
+		{
+			return a.errorId <= b.errorId;
+		}
+	};
+
 	struct ReportTargetInfo
 	{
 		langutil::ErrorId error;
 		langutil::SourceLocation location;
 		std::string message;
 	};
-	std::map<ASTNode const*, std::set<VerificationTargetType>, smt::EncodingContext::IdCompare> const& safeTargets() const { return m_safeTargets; }
+
+	std::map<ASTNode const*, std::set<CHCVerificationTarget>, smt::EncodingContext::IdCompare> const& safeTargets() const { return m_safeTargets; }
 	std::map<ASTNode const*, std::map<VerificationTargetType, ReportTargetInfo>, smt::EncodingContext::IdCompare> const& unsafeTargets() const { return m_unsafeTargets; }
 
 	/// This is used if the Horn solver is not directly linked into this binary.
@@ -261,8 +273,6 @@ private:
 	void verificationTargetEncountered(ASTNode const* const _errorNode, VerificationTargetType _type, smtutil::Expression const& _errorCondition);
 
 	void checkVerificationTargets();
-	// Forward declarations. Definitions are below.
-	struct CHCVerificationTarget;
 	struct CHCQueryPlaceholder;
 	void checkAssertTarget(ASTNode const* _scope, CHCVerificationTarget const& _target);
 	void checkAndReportTarget(
@@ -272,6 +282,8 @@ private:
 		std::string _satMsg,
 		std::string _unknownMsg = ""
 	);
+
+	std::pair<std::string, langutil::ErrorId> targetDescription(CHCVerificationTarget const& target);
 
 	std::optional<std::string> generateCounterexample(smtutil::CHCSolverInterface::CexGraph const& _graph, std::string const& _root);
 
@@ -370,12 +382,6 @@ private:
 
 	/// Verification targets.
 	//@{
-	struct CHCVerificationTarget: VerificationTarget
-	{
-		unsigned const errorId;
-		ASTNode const* const errorNode;
-	};
-
 	/// Query placeholder stores information necessary to create the final query edge in the CHC system.
 	/// It is combined with the unique error id (and error type) to create a complete Verification Target.
 	struct CHCQueryPlaceholder
@@ -398,7 +404,7 @@ private:
 	std::map<unsigned, CHCVerificationTarget> m_verificationTargets;
 
 	/// Targets proved safe.
-	std::map<ASTNode const*, std::set<VerificationTargetType>, smt::EncodingContext::IdCompare> m_safeTargets;
+	std::map<ASTNode const*, std::set<CHCVerificationTarget>, smt::EncodingContext::IdCompare> m_safeTargets;
 	/// Targets proved unsafe.
 	std::map<ASTNode const*, std::map<VerificationTargetType, ReportTargetInfo>, smt::EncodingContext::IdCompare> m_unsafeTargets;
 	/// Targets not proved.

atosbucket/NesClassic

libsolidity/formal/BMC.cpp
@@ -99,6 +99,26 @@ void BMC::analyze(SourceUnit const& _source, map<ASTNode const*, set<Verificatio
 			" Consider increasing the timeout per query."
 		);
 
+	if (!m_settings.showProvedSafe && !m_safeTargets.empty())
+		m_errorReporter.info(
+			6002_error,
+			"BMC: " +
+			to_string(m_safeTargets.size()) +
+			" verification condition(s) proved safe!" +
+			" Enable the model checker option \"show proved safe\" to see all of them."
+		);
+	else if (m_settings.showProvedSafe)
+		for (auto const& [node, targets]: m_safeTargets)
+			for (auto const& target: targets)
+				m_errorReporter.info(
+					2961_error,
+					node->location(),
+					"BMC: " +
+					targetDescription(target) +
+					" check is safe!"
+				);
+
+
 	// If this check is true, Z3 and CVC4 are not available
 	// and the query answers were not provided, since SMTPortfolio
 	// guarantees that SmtLib2Interface is the first solver, if enabled.
@@ -694,6 +714,30 @@ pair<vector<smtutil::Expression>, vector<string>> BMC::modelExpressions()
 
 /// Verification targets.
 
+string BMC::targetDescription(BMCVerificationTarget const& _target)
+{
+	if (
+		_target.type == VerificationTargetType::Underflow ||
+		_target.type == VerificationTargetType::Overflow
+	)
+	{
+		auto const* intType = dynamic_cast<IntegerType const*>(_target.expression->annotation().type);
+		if (!intType)
+			intType = TypeProvider::uint256();
+
+		if (_target.type == VerificationTargetType::Underflow)
+			return "Underflow (resulting value less than " + formatNumberReadable(intType->minValue()) + ")";
+		return "Overflow (resulting value larger than " + formatNumberReadable(intType->maxValue()) + ")";
+	}
+	else if (_target.type == VerificationTargetType::DivByZero)
+		return "Division by zero";
+	else if (_target.type == VerificationTargetType::Assert)
+		return "Assertion violation";
+	else if (_target.type == VerificationTargetType::Balance)
+		return "Insufficient funds";
+	solAssert(false);
+}
+
 void BMC::checkVerificationTargets()
 {
 	for (auto& target: m_verificationTargets)
@@ -762,13 +806,13 @@ void BMC::checkUnderflow(BMCVerificationTarget& _target)
 		intType = TypeProvider::uint256();
 
 	checkCondition(
+		_target,
 		_target.constraints && _target.value < smt::minValue(*intType),
 		_target.callStack,
 		_target.modelExpressions,
 		_target.expression->location(),
 		4144_error,
 		8312_error,
-		"Underflow (resulting value less than " + formatNumberReadable(intType->minValue()) + ")",
 		"<result>",
 		&_target.value
 	);
@@ -795,13 +839,13 @@ void BMC::checkOverflow(BMCVerificationTarget& _target)
 		intType = TypeProvider::uint256();
 
 	checkCondition(
+		_target,
 		_target.constraints && _target.value > smt::maxValue(*intType),
 		_target.callStack,
 		_target.modelExpressions,
 		_target.expression->location(),
 		2661_error,
 		8065_error,
-		"Overflow (resulting value larger than " + formatNumberReadable(intType->maxValue()) + ")",
 		"<result>",
 		&_target.value
 	);
@@ -818,13 +862,13 @@ void BMC::checkDivByZero(BMCVerificationTarget& _target)
 		return;
 
 	checkCondition(
+		_target,
 		_target.constraints && (_target.value == 0),
 		_target.callStack,
 		_target.modelExpressions,
 		_target.expression->location(),
 		3046_error,
 		5272_error,
-		"Division by zero",
 		"<result>",
 		&_target.value
 	);
@@ -834,13 +878,13 @@ void BMC::checkBalance(BMCVerificationTarget& _target)
 {
 	solAssert(_target.type == VerificationTargetType::Balance, "");
 	checkCondition(
+		_target,
 		_target.constraints && _target.value,
 		_target.callStack,
 		_target.modelExpressions,
 		_target.expression->location(),
 		1236_error,
 		4010_error,
-		"Insufficient funds",
 		"address(this).balance"
 	);
 }
@@ -856,13 +900,13 @@ void BMC::checkAssert(BMCVerificationTarget& _target)
 		return;
 
 	checkCondition(
+		_target,
 		_target.constraints && !_target.value,
 		_target.callStack,
 		_target.modelExpressions,
 		_target.expression->location(),
 		4661_error,
-		7812_error,
-		"Assertion violation"
+		7812_error
 	);
 }
 
@@ -894,13 +938,13 @@ void BMC::addVerificationTarget(
 /// Solving.
 
 void BMC::checkCondition(
+	BMCVerificationTarget const& _target,
 	smtutil::Expression _condition,
 	vector<SMTEncoder::CallStackEntry> const& _callStack,
 	pair<vector<smtutil::Expression>, vector<string>> const& _modelExpressions,
 	SourceLocation const& _location,
 	ErrorId _errorHappens,
 	ErrorId _errorMightHappen,
-	string const& _description,
 	string const& _additionalValueName,
 	smtutil::Expression const* _additionalValue
 )
@@ -942,7 +986,7 @@ void BMC::checkCondition(
 	{
 		solAssert(!_callStack.empty(), "");
 		std::ostringstream message;
-		message << "BMC: " << _description << " happens here.";
+		message << "BMC: " << targetDescription(_target) << " happens here.";
 
 		std::ostringstream modelMessage;
 		// Sometimes models have complex smtlib2 expressions that SMTLib2Interface fails to parse.
@@ -969,12 +1013,15 @@ void BMC::checkCondition(
 		break;
 	}
 	case smtutil::CheckResult::UNSATISFIABLE:
+	{
+		m_safeTargets[_target.expression].insert(_target);
 		break;
+	}
 	case smtutil::CheckResult::UNKNOWN:
 	{
 		++m_unprovedAmt;
 		if (m_settings.showUnproved)
-			m_errorReporter.warning(_errorMightHappen, _location, "BMC: " + _description + " might happen here.", secondaryLocation);
+			m_errorReporter.warning(_errorMightHappen, _location, "BMC: " + targetDescription(_target) + " might happen here.", secondaryLocation);
 		break;
 	}
 	case smtutil::CheckResult::CONFLICTING:

libsolidity/formal/BMC.h
@@ -135,7 +135,14 @@ private:
 		Expression const* expression;
 		std::vector<CallStackEntry> callStack;
 		std::pair<std::vector<smtutil::Expression>, std::vector<std::string>> modelExpressions;
+
+		friend bool operator<(BMCVerificationTarget const& _a, BMCVerificationTarget const& _b)
+		{
+			return _a.expression->id() < _b.expression->id();
+		}
 	};
+
+	std::string targetDescription(BMCVerificationTarget const& _target);
 
 	void checkVerificationTargets();
 	void checkVerificationTarget(BMCVerificationTarget& _target);
@@ -156,13 +163,13 @@ private:
 	//@{
 	/// Check that a condition can be satisfied.
 	void checkCondition(
+		BMCVerificationTarget const& _target,
 		smtutil::Expression _condition,
 		std::vector<CallStackEntry> const& _callStack,
 		std::pair<std::vector<smtutil::Expression>, std::vector<std::string>> const& _modelExpressions,
 		langutil::SourceLocation const& _location,
 		langutil::ErrorId _errorHappens,
 		langutil::ErrorId _errorMightHappen,
-		std::string const& _description,
 		std::string const& _additionalValueName = "",
 		smtutil::Expression const* _additionalValue = nullptr
 	);
@@ -188,7 +195,10 @@ private:
 
 	std::vector<BMCVerificationTarget> m_verificationTargets;
 
-	/// Targets that were already proven.
+	/// Targets proved safe by this engine.
+	std::map<ASTNode const*, std::set<BMCVerificationTarget>, smt::EncodingContext::IdCompare> m_safeTargets;
+
+	/// Targets that were already proven before this engine started.
 	std::map<ASTNode const*, std::set<VerificationTargetType>, smt::EncodingContext::IdCompare> m_solvedTargets;
 
 	/// Number of verification conditions that could not be proved.

libsolidity/formal/CHC.h
@@ -65,13 +65,25 @@ public:
 
 	void analyze(SourceUnit const& _sources);
 
+	struct CHCVerificationTarget: VerificationTarget
+	{
+		unsigned const errorId;
+		ASTNode const* const errorNode;
+
+		friend bool operator<(CHCVerificationTarget const& _a, CHCVerificationTarget const& _b)
+		{
+			return _a.errorId < _b.errorId;
+		}
+	};
+
 	struct ReportTargetInfo
 	{
 		langutil::ErrorId error;
 		langutil::SourceLocation location;
 		std::string message;
 	};
-	std::map<ASTNode const*, std::set<VerificationTargetType>, smt::EncodingContext::IdCompare> const& safeTargets() const { return m_safeTargets; }
+
+	std::map<ASTNode const*, std::set<CHCVerificationTarget>, smt::EncodingContext::IdCompare> const& safeTargets() const { return m_safeTargets; }
 	std::map<ASTNode const*, std::map<VerificationTargetType, ReportTargetInfo>, smt::EncodingContext::IdCompare> const& unsafeTargets() const { return m_unsafeTargets; }
 
 	/// This is used if the Horn solver is not directly linked into this binary.
@@ -261,8 +273,6 @@ private:
 	void verificationTargetEncountered(ASTNode const* const _errorNode, VerificationTargetType _type, smtutil::Expression const& _errorCondition);
 
 	void checkVerificationTargets();
-	// Forward declarations. Definitions are below.
-	struct CHCVerificationTarget;
 	struct CHCQueryPlaceholder;
 	void checkAssertTarget(ASTNode const* _scope, CHCVerificationTarget const& _target);
 	void checkAndReportTarget(
@@ -272,6 +282,8 @@ private:
 		std::string _satMsg,
 		std::string _unknownMsg = ""
 	);
+
+	std::pair<std::string, langutil::ErrorId> targetDescription(CHCVerificationTarget const& _target);
 
 	std::optional<std::string> generateCounterexample(smtutil::CHCSolverInterface::CexGraph const& _graph, std::string const& _root);
 
@@ -370,12 +382,6 @@ private:
 
 	/// Verification targets.
 	//@{
-	struct CHCVerificationTarget: VerificationTarget
-	{
-		unsigned const errorId;
-		ASTNode const* const errorNode;
-	};
-
 	/// Query placeholder stores information necessary to create the final query edge in the CHC system.
 	/// It is combined with the unique error id (and error type) to create a complete Verification Target.
 	struct CHCQueryPlaceholder
@@ -398,7 +404,7 @@ private:
 	std::map<unsigned, CHCVerificationTarget> m_verificationTargets;
 
 	/// Targets proved safe.
-	std::map<ASTNode const*, std::set<VerificationTargetType>, smt::EncodingContext::IdCompare> m_safeTargets;
+	std::map<ASTNode const*, std::set<CHCVerificationTarget>, smt::EncodingContext::IdCompare> m_safeTargets;
 	/// Targets proved unsafe.
 	std::map<ASTNode const*, std::map<VerificationTargetType, ReportTargetInfo>, smt::EncodingContext::IdCompare> m_unsafeTargets;
 	/// Targets not proved.